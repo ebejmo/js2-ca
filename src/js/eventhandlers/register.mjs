2a02ab2d5d0e431f9ce381d294397407b6679960
ebejmo <94139531+ebejmo@users.noreply.github.com>
2023-11-07 14:12:52 +0100

    reusable eventlistener

---
+++ src/js/eventhandlers/register.mjs
@@ -0,0 +1,19 @@
+import { register } from "../api/auth/register.mjs";
+
+export function setRegisterEventFormListener() {
+  const form = document.querySelector("#registerForm");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const formData = new FormData(form);
+    // creates an object
+    // entries gives keys and values
+    const profile = Object.fromEntries(formData.entries());
+    const action = form.action;
+    const method = form.method;
+
+    // api send
+    register(profile, action, method);
+  });
+}
